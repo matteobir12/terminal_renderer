9d1b5326ecf5f4dc19e3c480403189556c1a1f8d
matteo <bir.matteo@gmail.com>
2025-12-30 10:27:25 -0800

    move to mesh uts

--- src/rendering.rs
+++ src/rendering.rs
@@ -1,6 +1,8 @@
 mod gray_to_ascii;
 mod pipeline;
+mod mesh_utils;
 
 pub use self::gray_to_ascii::gray_to_ascii;
 pub use self::pipeline::Triangle;
 pub use self::pipeline::do_pipeline;
+pub use self::mesh_utils::*;
